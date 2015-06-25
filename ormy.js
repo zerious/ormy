a193739f8e328500b889a7a28f3a6fe27cae0d37
Sam Eubank <sameubank+zerious@gmail.com>
2015-06-25 12:00:13 -0700

    Beginnings of "standard" style

--- ormy.js
+++ ormy.js
@@ -1,56 +1,55 @@
-var Database = require(__dirname + '/lib/database');
-var Model = require(__dirname + '/lib/model');
-var Field = require(__dirname + '/lib/field');
+var Database = require(__dirname + '/lib/database')
+var Model = require(__dirname + '/lib/model')
+var Field = require(__dirname + '/lib/field')
 
 /**
  * The main API function accepts a configuration and returns a database.
  */
 var ormy = module.exports = function (config, app) {
-
   // Ensure there's a config.
-  config = config || {};
+  config = config || {}
 
   // Validate the log to ensure the desired methods exist.
-  var log = config.log = (config.log || console);
+  var log = config.log = (config.log || console)
   if (
-    typeof log.error != 'function' ||
-    typeof log.warn != 'function' ||
-    typeof log.info != 'function' ||
-    typeof log.log != 'function') {
-    throw new Error('[Ormy] Log must have error, warn, info and log methods.');
+    typeof log.error !== 'function' ||
+    typeof log.warn !== 'function' ||
+    typeof log.info !== 'function' ||
+    typeof log.log !== 'function') {
+    throw new Error('[Ormy] Log must have error, warn, info and log methods.')
   }
 
   // Adapters have lowercase names like "mysql".
-  var adapter = (config.adapter || 'mysql');
+  var adapter = (config.adapter || 'mysql')
 
-  var AdapterDatabase = ormy.adapters[adapter];
+  var AdapterDatabase = ormy.adapters[adapter]
   if (!AdapterDatabase) {
-    throw new Error('[Ormy] Unsupported database adapter: "' + adapter + '"');
+    throw new Error('[Ormy] Unsupported database adapter: "' + adapter + '"')
   }
 
   // Built-in adapters just have true values in the adapters collection.
   if (AdapterDatabase === true) {
-    AdapterDatabase = require(__dirname + '/lib/' + adapter + '-database');
+    AdapterDatabase = require(__dirname + '/lib/' + adapter + '-database')
   }
 
   // Instantiate a database from the adapter.
-  var db = new AdapterDatabase(config);
+  var db = new AdapterDatabase(config)
   if (app) {
-    db.app = app;
+    db.app = app
   }
 
   // Link to the appropriate field and model classes.
-  db.Field = db.Field || Field;
-  db.Model = db.Model || Model;
+  db.Field = db.Field || Field
+  db.Model = db.Model || Model
 
-  return db;
+  return db
 
-};
+}
 
 /**
  * Limit the maximum number of results that can be requested.
  */
-ormy._MAX_RESULTS = 1e5;
+ormy._MAX_RESULTS = 1e5
 
 /**
  * Supported database adapters.
@@ -59,12 +58,12 @@ ormy.adapters = {
   mysql: true,
   ringer: true,
   sqlite: true
-};
+}
 
 // Expose the version number, but only load package JSON if a get is performed.
 Object.defineProperty(ormy, 'version', {
   enumerable: false,
   get: function () {
-    return require(__dirname + '/package.json').version;
+    return require(__dirname + '/package.json').version
   }
-});
+})
--- test/sqlite-test.js
+++ test/sqlite-test.js
@@ -1,6 +1,6 @@
-var up = __dirname.replace(/[\/\\][^\/\\]+$/, '');
-var ormy = require(up + '/ormy');
-var Model = require(up + '/lib/model');
+var up = __dirname.replace(/[\/\\][^\/\\]+$/, '')
+var ormy = require(up + '/ormy')
+var Model = require(up + '/lib/model')
 
 describe('SQLite', function () {
   var log = {
@@ -8,13 +8,13 @@ describe('SQLite', function () {
     warn: mock.concat(),
     info: mock.concat(),
     log: mock.concat()
-  };
+  }
   var db = ormy({
     adapter: 'sqlite',
     path: ':memory:',
     name: 'test',
     log: log
-  });
+  })
   var Mock = db.define({
     table: 'mocks',
     fields: {
@@ -28,66 +28,66 @@ describe('SQLite', function () {
       enums: 'enum("this","is","important")'
     },
     enableSync: false
-  });
+  })
 
   it('should create table', function (done) {
-    db.sync(Mock, done);
-  });
+    db.sync(Mock, done)
+  })
 
   it('should update table', function (done) {
-    db.sync(Mock, done);
-  });
+    db.sync(Mock, done)
+  })
 
   it('should create a first record', function (done) {
-    var expected = { name: 'what is this', number: 12, tiny: 1, big: 1, small: 1, big: 1, money: 10.20, enums: 'is', id: 1};
+    var expected = { name: 'what is this', number: 12, tiny: 1, big: 1, small: 1, big: 1, money: 10.20, enums: 'is', id: 1}
     Mock.save({ name: 'what is this', number: 12, tiny: 1, big: 1, small: 1, big: 1, money: 10.20, enums: 'is'}, function (err, item) {
-      is.falsy(err);
-      // delete created, modified, deleted columns since it is dynamic
-      ['deleted', 'created', 'modified']
-      .forEach(function (key) {
-        delete item[key];
-      });
-      is.same(item, expected);
-      done();
-    });
-  });
+      is.falsy(err)
+        // delete created, modified, deleted columns since it is dynamic
+        var autoFields = ['deleted', 'created', 'modified']
+        autoFields.forEach(function (key) {
+          delete item[key]
+        })
+      is.same(item, expected)
+      done()
+    })
+  })
 
   it('should get a first record', function (done) {
-    var expected = { id: 1, name: 'what is this', number: 12, tiny: 1, big: 1, small: 1, big: 1, money: 10.20, enums: 'is'};
+    var expected = { id: 1, name: 'what is this', number: 12, tiny: 1, big: 1, small: 1, big: 1, money: 10.20, enums: 'is'}
     Mock.get(1, function (err, item) {
-      is.falsy(err);
-      // delete created, modified, deleted columns since it is dynamic
-      ['deleted', 'created', 'modified']
-      .forEach(function (key) {
-        delete item[key];
-      });
-      is.same(expected, item);
-      done();
-    });
-  });
+      is.falsy(err)
+        // delete created, modified, deleted columns since it is dynamic
+        var autoFields = ['deleted', 'created', 'modified']
+        autoFields.forEach(function (key) {
+          delete item[key]
+        })
+      is.same(expected, item)
+      done()
+    })
+  })
 
   it('should update a first record', function (done) {
     Mock.get(1, function (err, item) {
-      is.falsy(err);
-      var old = item.modified;
-      var beforeUpdate = item;
-      beforeUpdate.number = 24;
+      is.falsy(err)
+      var old = item.modified
+      var beforeUpdate = item
+      beforeUpdate.number = 24
       Mock.save(beforeUpdate, function (err, afterUpdate) {
-        is.null(err);
-        is.same(beforeUpdate.number, afterUpdate.number);
-        done();
-      });
-    });
-  });
+        is.null(err)
+        is.same(beforeUpdate.number, afterUpdate.number)
+        done()
+      })
+    })
+  })
 
   it('should remove a first record', function (done) {
     Mock.remove(1, function (err) {
-      is.falsy(err);
+      is.falsy(err)
       Mock.get(1, function (err, item) {
-        is.falsy(err);
-        is.undefined(item);
-        done();
-      });
-    });
+        is.falsy(err)
+        is.undefined(item)
+        done()
+      })
+    })
   })
-});
+})
